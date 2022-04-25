22dad6e7d27b74dd1b7275117df1f8b490580d74
n00bNn00b <reldewan9@gmail.com>
2022-04-25 21:05:03 +0600

    Footer has been updated and Login component has been created

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
+  const startingYear = 2021;
+  const presentYear = new Date().getFullYear();
   return (
     <footer className="mt-1">
-      <div>Footer</div>
+      <div className="text-center">
+        &copy; {startingYear} - {presentYear} | All Rights Reserved.
+      </div>
     </footer>
   );
 };
